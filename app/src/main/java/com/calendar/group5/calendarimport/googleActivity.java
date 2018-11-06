190f9f3f212d6bdca6b66c044656c2dc2ea197a3
Brandon <brandonburchett@gmail.com>
2018-11-06 00:03:23 -0600

    calendar stuff

--- app/src/main/java/com/calendar/group5/calendarimport/googleActivity.java
+++ app/src/main/java/com/calendar/group5/calendarimport/googleActivity.java
@@ -15,8 +15,35 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Activity to demonstrate basic retrieval of the Google user's ID, email address, and basic
@@ -30,6 +57,12 @@ public class googleActivity extends AppCompatActivity implements
 
     private GoogleSignInClient mGoogleSignInClient;
     private TextView mStatusTextView;
+    private static final String APPLICATION_NAME = "CalendarImport";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+    private static final String CREDENTIALS_FILE_PATH = "./client_secret.json";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +82,7 @@ public class googleActivity extends AppCompatActivity implements
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestScopes(new Scope("https://www.googleapis.com/auth/calendar"))
                 .build();
         // [END configure_signin]
 
@@ -65,15 +99,18 @@ public class googleActivity extends AppCompatActivity implements
         // [END customize_button]
     }
 
+
     @Override
     public void onStart() {
         super.onStart();
+
 
         // [START on_start_sign_in]
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         updateUI(account);
+
         // [END on_start_sign_in]
     }
 
@@ -88,6 +125,7 @@ public class googleActivity extends AppCompatActivity implements
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+
         }
     }
     // [END onActivityResult]
@@ -97,8 +135,61 @@ public class googleActivity extends AppCompatActivity implements
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
+
+
             // Signed in successfully, show authenticated UI.
             updateUI(account);
+            try {
+                 NetHttpTransport HTTP_TRANSPORT =  new com.google.api.client.http.javanet.NetHttpTransport();
+
+
+                Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
+
+
+                Event event = new Event()
+                        .setSummary("Google I/O 2015")
+                        .setLocation("800 Howard St., San Francisco, CA 94103")
+                        .setDescription("A chance to hear more about Google's developer products.");
+
+                DateTime startDateTime = new DateTime("2018-11-27T09:00:00-07:00");
+                EventDateTime start = new EventDateTime()
+                        .setDateTime(startDateTime)
+                        .setTimeZone("America/Los_Angeles");
+                event.setStart(start);
+
+                DateTime endDateTime = new DateTime("2018-11-28T17:00:00-07:00");
+                EventDateTime end = new EventDateTime()
+                        .setDateTime(endDateTime)
+                        .setTimeZone("America/Los_Angeles");
+                event.setEnd(end);
+
+                String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+                event.setRecurrence(Arrays.asList(recurrence));
+
+                EventAttendee[] attendees = new EventAttendee[] {
+                        new EventAttendee().setEmail("lpage@example.com"),
+                        new EventAttendee().setEmail("sbrin@example.com"),
+                };
+                event.setAttendees(Arrays.asList(attendees));
+
+                EventReminder[] reminderOverrides = new EventReminder[] {
+                        new EventReminder().setMethod("email").setMinutes(24 * 60),
+                        new EventReminder().setMethod("popup").setMinutes(10),
+                };
+                Event.Reminders reminders = new Event.Reminders()
+                        .setUseDefault(false)
+                        .setOverrides(Arrays.asList(reminderOverrides));
+                event.setReminders(reminders);
+
+                String calendarId = "primary";
+                service.events().insert(calendarId, event).execute();
+                Log.d(TAG,"event created");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -170,6 +261,20 @@ public class googleActivity extends AppCompatActivity implements
                 revokeAccess();
                 break;
         }
+    }
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = googleActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receier = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receier).authorize("user");
     }
 }
 
